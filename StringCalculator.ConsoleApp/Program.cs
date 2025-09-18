17b2f5a1c91c021dce4ee766cbc36947be6d33ae
Anders Branderud <anders.branderud@gmail.com>
2025-09-18 10:39:48 +0200

    program refactor

--- StringCalculator.ConsoleApp/Program.cs
+++ StringCalculator.ConsoleApp/Program.cs
@@ -7,10 +7,25 @@ namespace StringCalculator.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter numbers to add (comma separated):");
-            string input = Console.ReadLine();
-            int result = StringCalculatorUtility.Add(input);
-            Console.WriteLine($"Result: {result}");
+            Console.WriteLine("Welcome to StringCalculator.");
+            Console.WriteLine("Enter x to exit.");
+
+            while (true)
+            {
+                Console.WriteLine("Enter numbers to add (comma separated):");
+
+                string input = Console.ReadLine();
+                int result = StringCalculatorUtility.Add(input);
+
+                if (input == "x")
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine($"Result of the numbers: {result}");
+                }
+            }
         }
     }
 }
